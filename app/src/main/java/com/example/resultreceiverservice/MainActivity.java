f5fe13fee6841ba1adb2d09b6eb9093508e4efe2
Bilal ÖZ <152805013@ogr.cbu.edu.tr>
2020-09-01 15:43:14 +0300

    Create Layout

--- app/src/main/java/com/example/resultreceiverservice/MainActivity.java
+++ app/src/main/java/com/example/resultreceiverservice/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.resultreceiverservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.txtAddress);
+        textView = findViewById(R.id.txtResult);
+    }
+
+    public void download(View view) {
+
     }
 }
